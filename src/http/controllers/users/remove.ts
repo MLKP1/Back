94e366ac1c9b69285b29a9f10f60b44edfdb0d86
lucaslinyker <lucaslinyker@outlook.com>
2025-06-26 15:25:10 -0300

    refact: Adicionar logout ao deletar a conta

--- src/http/controllers/users/remove.ts
+++ src/http/controllers/users/remove.ts
@@ -3,6 +3,8 @@ import type { FastifyReply, FastifyRequest } from 'fastify'
 import { UserNotExistsError } from '@/services/errors/user-not-exists-error'
 import { makeRemoveService } from '@/services/factories/make-remove-service'
 
+import { logout } from './logout'
+
 export async function remove(request: FastifyRequest, reply: FastifyReply) {
   const id = request.user.sub
 
@@ -10,6 +12,8 @@ export async function remove(request: FastifyRequest, reply: FastifyReply) {
     const removeService = makeRemoveService()
 
     await removeService.execute({ id })
+
+    await logout(request, reply)
   } catch (err) {
     if (err instanceof UserNotExistsError) {
       return reply.status(404).send({ message: err.message })
